131c85c405d651851e68968fc2a32858c0e53f48
Pedro Germano <pedrogermano232@gmail.com>
2023-11-14 14:44:54 -0300

    arrow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 let slide_combos = new Swiper('.slide-combos', {
-  slidesPerView: 3
+  slidesPerView: 3,
+  spaceBetween: 32,
+  speed: 800,
+  pagination: {
+    el: '',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '',
+    prevEl: ''
+  }
 })
+
+  
